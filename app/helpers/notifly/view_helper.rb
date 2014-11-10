90ad295f5ac29393b0f1e3a4e3142aa8a7c0717c
Pedro Passalini <henrique.passalini@gmail.com>
2014-11-10 16:11:11 -0200

    Refactoring counter notifications

--- app/helpers/notifly/view_helper.rb
+++ app/helpers/notifly/view_helper.rb
@@ -1,5 +1,9 @@
 module Notifly
   module ViewHelper
+    def notiflies
+      notiflies_for current_user
+    end
+
     def notiflies_for(receiver)
       render partial: 'notifly/notiflies', locals: { receiver: receiver }
     end
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    DummyObject.first
+  end
+
+  ActiveSupport.on_load(:action_controller) do
+    helper_method :current_user
+  end
 end
