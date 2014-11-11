618ee545fcb825b9c0c4f227ad91b630df5c6d80
Pedro Passalini <henrique.passalini@gmail.com>
2014-11-11 16:36:46 -0200

    trying to inject migrations in the main_app

--- lib/notifly/engine.rb
+++ lib/notifly/engine.rb
@@ -9,5 +9,13 @@ module Notifly
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
