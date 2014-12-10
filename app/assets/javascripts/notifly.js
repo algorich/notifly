9449a4ddd45a34012b8f64442b28d76763b95720
Pedro Passalini <henrique.passalini@gmail.com>
2014-12-10 13:44:26 -0200

    Add Notification's counter to App's favicon (closes #40)

--- app/assets/javascripts/notifly.js
+++ app/assets/javascripts/notifly.js
@@ -1,5 +1,6 @@
 //= require 'jquery'
 //= require 'jquery_ujs'
+//= require 'tinycon'
 //= require 'notifly/get_notifications'
 //= require 'notifly/seen_notifications'
 //= require 'notifly/read_notifications'
@@ -9,4 +10,6 @@ $(document).ready(function() {
   $(document).on('click', '#notifly-notifications-panel.dropdown-menu', function (e) {
     $('#notifly').hasClass('keep_open') && e.stopPropagation();
   });
+
+  Tinycon.setBubble(0);
 });
