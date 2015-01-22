e3179fe3055fe7e94f36a282219594f6ae08a7f1
Pedro Passalini <henrique.passalini@gmail.com>
2015-01-22 09:21:13 -0200

    trying to fix specs

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ActiveRecord::Base
   notifly before: :destroy, template: :destroy, data: :attributes
 
   notifly after: :publish!, template: :publish
-  notifly before: :change_title, data: -> { { title_before_create: title } },
-    unless: -> { title == 'TitleFoo' }
+  notifly before: :change_title, data: -> { { title_before_create: self.title } },
+    unless: -> { self.title == 'TitleFoo' }
   notifly after:  :change_title, data: :attributes, template: :change_title,
     if: -> { self.title == 'NewTitle' }
 
